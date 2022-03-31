40af8cedef7cd82fa0a846faf26ee7c76f5d727f
Michel Michels <michel.michels.jr@gmail.com>
2022-03-31 16:25:28 +0200

    WhoAmI project added

---
+++ WhoAmI/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MichelMichels.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoAmI
+{
+    public class MainViewModel : BasePropertyChanged
+    {
+        public MainViewModel()
+        {
+            Username = Environment.UserName;
+        }
+
+        public string Username { get; set; }
+    }
+}
